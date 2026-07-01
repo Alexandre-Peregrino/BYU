1478a940dc5b0298cf77381ce5387f70b7013ed7
ALEXANDRE CLEITON LIMA PEREGRINO <alexandre.lima.914@sesed.interno>
2026-07-01 13:37:46 -0300

    Primeiro commit da história

---
+++ historias_inteligentes.py
@@ -0,0 +1,11 @@
+print('Por favor, digite o seguinte: ')
+
+adjetivo = input('adjetivo: ')
+animal = input('animal: ')
+verbo1 = input('verbo: ')
+exclamacao = input('exclamação: ')
+verbo2 = input('verbo: ')
+verbo3 = input('verbo: ')
+
+print(f'''Outro dia, eu estava realmente em apuros. Tudo começou quando eu vi um {animal} {adjetivo} {verbo1} pelo corredor. Eu gritei "{exclamacao.capitalize()}!". Mas tudo que eu conseguia pensar em fazer era {verbo2} repetidamente. Milagrosamente,
+isso fez com que ele parasse, mas não antes de tentar {verbo3} bem na frente da minha família.''')
