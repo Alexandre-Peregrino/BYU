85b5bf4f0c0680129e283fbc68a58c14c5a00097
Alexandre-Peregrino <160164397+Alexandre-Peregrino@users.noreply.github.com>
2026-07-02 19:30:05 -0300

    modificações

---
+++ variaveis_numericas.py
@@ -0,0 +1,17 @@
+#Exercício 01
+
+# idade = int(input('Informe sua idade: '))
+
+# print(f'Sua idade será: {idade + 1} anos no próximo aniversário')
+
+# #Exercício 02
+
+# caixas = int(input('Informe a quantidade de caixas: '))
+# print(f'O total de ovos é: {caixas * 12}')
+
+#Exercício 03
+
+biscoitos = int(input('Informe a quantidade de biscoitos: '))
+pessoas = int(input('Informe a quantidade de pessoas: '))
+
+print(f'O total de biscoitos por pessoa é: {biscoitos / pessoas}')
