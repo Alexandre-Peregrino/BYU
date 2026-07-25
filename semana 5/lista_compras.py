a2fb7f41d862c8ce6a907cde883b77c40502f153
Alexandre Peregrino <alexclp1977@gmail.com>
2026-07-25 19:06:31 -0300

    asa

---
+++ semana 5/lista_compras.py
@@ -0,0 +1,33 @@
+#Lista de compras
+
+compras =[]
+
+while True:
+    item_compra =input("Informe o ítem: ")
+    if item_compra.lower() == "fim":
+        break
+    compras.append(item_compra)
+
+print()
+
+print(f'A lista de compras é: {compras}')
+
+print()
+for i in range(len(compras)):
+    print(f'indice {i}, item {compras[i]}')
+
+print()
+
+remover_item = input('Qual ítem deseja remover? ')
+verifica = False
+for i in range(len(compras)):
+    if remover_item.lower() == compras[i].lower():
+        compras.pop(i)
+        inserir_item = input('Qual ítem deseja inserir? ')
+        compras.insert(i, inserir_item)
+        verifica = True
+        break
+if verifica:
+        print(f'Sua nova lista é: {compras}')       
+else:
+    print('Ítem não encontrado')
---
+++ semana 5/teste.py
@@ -0,0 +1,9 @@
+maior_valor = 0
+
+for valor in minha_lista:
+
+    if valor < maior_valor:
+
+        maior_valor = valor
+
+print(f'O maior valor é {maior_valor}') 
